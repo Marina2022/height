f3b4e20e14645d65a574460e830e3f11e52b9c7c
Marina2022 <marusiiiia@yandex.ru>
2023-10-17 19:24:50 +0300

    checkboxes ok

--- main.js
+++ main.js
@@ -2,18 +2,19 @@ const sidebarDropdown = document.querySelector('.sidebar-dropdown')
 const sidebarCollapsable = document.querySelector('.sidebar-collapsable')
 const dropdownArrow = document.querySelector('.dropdown-arrow')
 const collapseRowsBtns = document.querySelectorAll('.collapse-rows-btn')
-const editBtn = document.querySelector('.edit-btn')
+const editBtns = document.querySelectorAll('.edit-btn')
+
 
 // const dropDownContainer = document.querySelector('.drop-down-container')
 // const
 
-sidebarDropdown.addEventListener('click', ()=>{
+sidebarDropdown.addEventListener('click', () => {
   sidebarCollapsable.classList.toggle('hidden')
   dropdownArrow.classList.toggle('rotated')
 })
 
-collapseRowsBtns.forEach((collapseRowsBtn)=>{
-  collapseRowsBtn.addEventListener('click', (e)=>{
+collapseRowsBtns.forEach((collapseRowsBtn) => {
+  collapseRowsBtn.addEventListener('click', (e) => {
     const dropdown = e.target.closest('.drop-down-container').querySelector('.collapse-container')
     dropdown.classList.toggle('hidden')
     collapseRowsBtn.classList.toggle('rotated-row')
@@ -21,26 +22,28 @@ collapseRowsBtns.forEach((collapseRowsBtn)=>{
 })
 
 
+editBtns.forEach((editBtn) => {
+  editBtn.addEventListener('click', (e) => {
+    const container = e.target.closest('.edit-container')
+    const row = container.querySelector('.static-row')
+    const input = container.querySelector('.edit-input')
+    const text = row.querySelector('.text')
 
-editBtn.addEventListener('click', (e)=>{
+    const onBlur = (ev) => {
+      ev.target.classList.toggle('hidden')
+      row.classList.toggle('hidden')
+      text.textContent = input.value
+      ev.target.removeEventListener('blur', onBlur)
+    }
 
-  const container = e.target.closest('.edit-container')
-  const row = container.querySelector('.static-row')
-  const input = container.querySelector('.edit-input')
-  const text = row.querySelector('.text')
+    input.classList.toggle('hidden')
+    input.focus()
+    input.value = text.textContent
 
-  const onBlur = (ev)=> {
-    ev.target.classList.toggle('hidden')
     row.classList.toggle('hidden')
-    text.textContent = input.value
-    ev.target.removeEventListener('blur', onBlur)
-  }
 
-  input.classList.toggle('hidden')
-  input.focus()
-  input.value = text.textContent
-
-  row.classList.toggle('hidden')
-
-  input.addEventListener('blur', onBlur)
+    input.addEventListener('blur', onBlur)
+  })
 })
+
+
